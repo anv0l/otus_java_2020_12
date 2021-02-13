3128934e2253c0d7cbafdf2887346b9ac4f3406a
anv0l <andrei.voloshin@live.com>
2021-02-13 16:31:32 +0300

    1. Тип аннотации логгирования сделал Class<? extends Annotation>. 2. Вынес получение названия и параметров метода в отдельный метод. 3. Методы теперь сохраняются в HashSet.

--- hw05-aop/src/main/java/homework/CalculatorProxy.java
+++ hw05-aop/src/main/java/homework/CalculatorProxy.java
@@ -1,15 +1,16 @@
 package homework;
 
+import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 public class CalculatorProxy {
-    private static final Class LOGGER_ANNOTATION = Log.class;
-    private static List<String> logMethods;
+    private static final Class<? extends Annotation> LOGGER_ANNOTATION = Log.class;
+    private static Set<String> logMethods;
 
     private CalculatorProxy() {
     }
@@ -20,9 +21,13 @@ public class CalculatorProxy {
         Method[] methods = MyCalculator.class.getMethods();
         logMethods = Arrays.stream(methods)
                 .filter(method -> method.isAnnotationPresent(LOGGER_ANNOTATION))
-                .map(method -> method.getName() + Arrays.asList(method.getParameterTypes()).toString())
-                .collect(Collectors.toList());
+                .map(CalculatorProxy::getMethodParams)
+                .collect(Collectors.toSet());
         return (Calculator) Proxy.newProxyInstance(CalculatorProxy.class.getClassLoader(), new Class<?>[]{Calculator.class}, handler);
+    }
+
+    private static String getMethodParams(Method method) {
+        return method.getName() + Arrays.asList(method.getParameterTypes()).toString();
     }
 
     static class ProxyInvocationHandler implements InvocationHandler {
@@ -31,7 +36,7 @@ public class CalculatorProxy {
         ProxyInvocationHandler(Calculator myCalculator) {this.myCalculator = myCalculator;}
 
         private void log(Method method, Object[] args) {
-            String methodParams = method.getName() + Arrays.asList(method.getParameterTypes()).toString();
+            String methodParams = getMethodParams(method);
             if (logMethods.contains(methodParams)) {
                 String arguments = String.join(",", Arrays.asList(args).toString());
                 System.out.println("Залоггировано: " + method.getName() + arguments);

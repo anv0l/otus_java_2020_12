b4fee9f3abd8b828c93863f26f9e54a170dd0503
anv0l <andrei.voloshin@live.com>
2021-04-10 22:04:06 +0300

    ДЗ 10: Использование Hibernate
    Цель:
    На практике освоить основы Hibernate. Понять как аннотации-hibernate влияют на формирование sql-запросов.
    
    Работа должна использовать базу данных в docker-контейнере .
    
    За основу возьмите пример из вебинара про Hibernate (class DbServiceDemo).
    Добавьте в Client поля:
    * адрес (OneToOne) class AddressDataSet { private String street; }
    * телефон (OneToMany) class PhoneDataSet { private String number; }
    
    Разметьте классы таким образом, чтобы при сохранении/чтении объека Client каскадно сохранялись/читались вложенные объекты.
    
    ВАЖНО.
    
    Hibernate должен создать только три таблицы: для телефонов, адресов и клиентов.
    При сохранении нового объекта не должно быть update-ов. Посмотрите в логи и проверьте, что эти два требования выполняются.

---
+++ hw10-hibernate/src/main/java/ru/otus/crm/model/Client.java
@@ -0,0 +1,102 @@
+package ru.otus.crm.model;
+
+
+import org.hibernate.annotations.Cascade;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Entity
+@Table(name = "client")
+public class Client implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "address_id", referencedColumnName = "id")
+    private AddressDataSet address;
+
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private List<PhoneDataSet> phones;
+
+    public Client() {
+    }
+
+    public Client(String name) {
+        this.id = null;
+        this.name = name;
+    }
+
+    public Client(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Client(Long id, String name, AddressDataSet address, List<PhoneDataSet> phoneDataSet) {
+        this.id = id;
+        this.name = name;
+        this.address = address;
+        this.phones = phoneDataSet;
+    }
+
+    @Override
+    public Client clone() {
+        var client = new Client(this.getId(), this.getName());
+        client.setAddress(this.getAddress().clone());
+        client.setPhones(new ArrayList<>(this.getPhones().stream()
+                .map(PhoneDataSet::clone)
+                .collect(Collectors.toList())));
+        return this;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public AddressDataSet getAddress() {
+        return new AddressDataSet(address.getId(), address.getStreet());
+    }
+
+    public void setAddress(AddressDataSet address) {
+        this.address = address;
+    }
+
+    public List<PhoneDataSet> getPhones() {
+        return new ArrayList<>(this.phones) ;
+    }
+
+    public void setPhones(List<PhoneDataSet> phones) {
+        this.phones = phones;
+    }
+
+    @Override
+    public String toString() {
+        return "Client{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", address = '" + address.getStreet() + "'" +
+                ", phone(s) = '" + Arrays.toString(phones.toArray()) + "'" +
+                '}';
+    }
+}
